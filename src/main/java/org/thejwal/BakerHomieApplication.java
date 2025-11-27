52c4165379bb8cfde8a8ad1e989b228df7405d5d
Thejwal <thejwal.pavithran@gmail.com>
2025-11-27 13:12:36 +0100

    Baker Homie

---
+++ src/main/java/org/thejwal/BakerHomieApplication.java
@@ -0,0 +1,12 @@
+package org.thejwal;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakerHomieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BakerHomieApplication.class, args);
+    }
+}
